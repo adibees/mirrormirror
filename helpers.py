f43b93fde1243de5cf4d9b9138dd5250e547c030
Adhya Beesam <40211850+adibees@users.noreply.github.com>
2021-04-26 16:27:38 -0400

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -209,30 +209,7 @@ history = model.fit(
 )
 
 
-def sentiment(score, include_neutral=True):
-    if include_neutral:
-        label = NEUTRAL
-        if score <= SENTIMENT_THRESHOLDS[0]:
-            label = NEGATIVE
-        elif score >= SENTIMENT_THRESHOLDS[1]:
-            label = POSITIVE
-
-        return label
-    else:
-        return NEGATIVE if score < 0.5 else POSITIVE
-
-
-def predict(text, include_neutral=True):
-    start_at = time.time()
-    # Tokenize text
-    x_test = pad_sequences(tokenizer.texts_to_sequences([text]), maxlen=SEQUENCE_LENGTH)
-    # Predict
-    score = model.predict([x_test])[0]
-    # Decode sentiment
-    label = sentiment(score, include_neutral=include_neutral)
-
-    return {
-        "label": label,
-        "score": float(score),
-        "elapsed_time": time.time() - start_at,
-    }
+model.save(KERAS_MODEL)
+w2v_model.save(WORD2VEC_MODEL)
+pickle.dump(tokenizer, open(TOKENIZER_MODEL, "wb"), protocol=0)
+pickle.dump(encoder, open(ENCODER_MODEL, "wb"), protocol=0)
